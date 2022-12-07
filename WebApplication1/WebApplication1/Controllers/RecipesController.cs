a3ab764b22a379989d4599d828df24f8ba8f510b
unknown <paatatepnadze1@gmail.com>
2022-12-07 13:28:29 +0100

    web api binding

---
+++ WebApplication1/WebApplication1/Controllers/RecipesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")] //api/recipies
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetRecipies([FromQuery]int count)
+        {
+            string[] recipes = { "Buglama", "Chicken", "Donner" };
+            if (!recipes.Any())
+            { 
+                return NotFound(); 
+            }
+            return Ok(recipes.Take(count));
+        }
+
+
+       // [HttpPost]
+      //  public ActionResult CreateRecipies()
+       
+        
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteRecipies(string id) 
+        {
+            bool badThingsHappened = false;
+
+            if(badThingsHappened)
+                return BadRequest();
+
+            return NoContent();
+            
+
+        }
+    }
+}
